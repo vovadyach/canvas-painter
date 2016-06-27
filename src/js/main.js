17f237c1268dc1c674c214ac2706032e10bf375d
vinside <diachenko.vladimir@gmail.com>
2016-06-28 01:41:11 +0300

    Add css styles to body. Add evetListeners  to canvas code.

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,34 @@
 
  */
 
-function setup() {
+function onReady() {
+    var canvas = document.getElementById("canvas-1"),
+        context = canvas.getContext("2d"),
+        rect = canvas.getBoundingClientRect(),
+        mouseX,
+        mouseY;
 
+    canvas.addEventListener("mousedown", onMouseDown);
+
+    function onMouseDown(event) {
+        mouseX = event.clientX - rect.left;
+        mouseY = event.clientY - rect.top;
+        canvas.addEventListener("mousemove", onMouseMove);
+        document.body.addEventListener('mouseup', onMouseUp);
+    }
+
+    function onMouseMove(event) {
+        context.beginPath();
+        context.moveTo(mouseX, mouseY);
+        mouseX = event.clientX - rect.left;
+        mouseY = event.clientY - rect.top;
+        context.lineTo(mouseX, mouseY);
+        context.stroke();
+    }
+
+    function onMouseUp(event) {
+        canvas.removeEventListener("mousemove", onMouseMove);
+        document.body.removeEventListener( 'mouseup', onMouseUp);
+    }
 }
-
-window.onload = setup;
+window.onload = onReady;
