980bac8ea5bafbe2320ffab5a5d2d3c03d32729c
Vladimir Dyachenko <vovickdyachenko@gmail.com>
2016-06-29 01:30:58 +0300

    Added div for canvas

--- src/js/main.js
+++ src/js/main.js
@@ -1,91 +1,100 @@
 /**
  * Created by vins on 28.06.2016.
-
  */
 
 function onReady() {
-    var buttonnode = document.createElement('input');
-          buttonnode.setAttribute('class', 'red');
-          buttonnode.setAttribute('data-color', '#ff0000');
-          buttonnode.setAttribute('type','button');
-          buttonnode.setAttribute('value','Red');
-          document.body.appendChild(buttonnode);
+  var buttonnode = document.createElement('input');
+  buttonnode.setAttribute('class', 'red');
+  buttonnode.setAttribute('data-color', '#ff0000');
+  buttonnode.setAttribute('type','button');
+  buttonnode.setAttribute('value','Red');
+  document.body.appendChild(buttonnode);
 
-          var buttonnode = document.createElement('input');
-          buttonnode.setAttribute('class', 'green');
-          buttonnode.setAttribute('data-color', '#3dae49');
-          buttonnode.setAttribute('type','button');
-          buttonnode.setAttribute('value','Green');
-          document.body.appendChild(buttonnode);
+  var buttonnode = document.createElement('input');
+  buttonnode.setAttribute('class', 'green');
+  buttonnode.setAttribute('data-color', '#3dae49');
+  buttonnode.setAttribute('type','button');
+  buttonnode.setAttribute('value','Green');
+  document.body.appendChild(buttonnode);
 
-          var buttonnode = document.createElement('input');
-          buttonnode.setAttribute('class', 'blue');
-          buttonnode.setAttribute('data-color', '#009cc5');
-          buttonnode.setAttribute('type','button');
-          buttonnode.setAttribute('value','Blue');
-          document.body.appendChild(buttonnode);
+  var buttonnode = document.createElement('input');
+  buttonnode.setAttribute('class', 'blue');
+  buttonnode.setAttribute('data-color', '#009cc5');
+  buttonnode.setAttribute('type','button');
+  buttonnode.setAttribute('value','Blue');
+  document.body.appendChild(buttonnode);
 
-          var clearBox = document.createElement('div');
-          clearBox.setAttribute('id', 'clear-box');
-          document.body.appendChild(clearBox);
-          
-          var clearBtn = document.createElement('input');
-          clearBtn.setAttribute('id', 'clear');
-          clearBtn.setAttribute('type','button');
-          clearBtn.setAttribute('value','Clear');
-          document.body.appendChild(clearBtn);
+  var clearBox = document.createElement('div');
+  clearBox.setAttribute('id', 'clear-box');
+  document.body.appendChild(clearBox);
+  
+  var clearBtn = document.createElement('input');
+  clearBtn.setAttribute('id', 'clear');
+  clearBtn.setAttribute('type','button');
+  clearBtn.setAttribute('value','Clear');
+  document.body.appendChild(clearBtn);
 
-          var canvas = document.createElement('canvas');
-          var widthCanv = 1200;
-          var heightCanv = 640;
+  var canvasBox = document.createElement('div');
+  clearBox.setAttribute('id', 'canvas-box');
+  document.body.appendChild(canvasBox);
 
-          canv.id = 'someId';
-          document.body.appendChild(canvas);
-          
-          canv.width = widthCanv;
-          canv.height = heightCanv;
+  var canvas = document.createElement('canvas');
+  var widthCanv = 1200;
+  var heightCanv = 640;
+
+  canvas.id = 'canvas';
+  document.body.appendChild(canvas);
+  //Places clear button inside a div  
+  var canvasGenerate = document.getElementById("canvas");
+  canvasBox.insertBefore(canvasBox, canvasGenerate.firstChild);
+
+  canvas.width = widthCanv;
+  canvas.height = heightCanv;
+
+  var rect = canvas.getBoundingClientRect();
+  var mouseX;
+  var mouseY;
     
-        var rect = canvas.getBoundingClientRect();
-        var mouseX;
-        var mouseY;
+  var context = canvas.getContext("2d");
+  context.beginPath();
 
-          
-          var context = canv.getContext("2d");
-          context.beginPath();
-          context.strokeRect(12,20,1185, 600);
+  context.strokeRect(0,0,1185, 600);
 
-          //Places clear button inside a div  
-          var generateHere = document.getElementById("clear-box");
-          generateHere.insertBefore(clearBtn, generateHere.firstChild);
+  //Places clear button inside a div  
+  var clearBoxGenerate = document.getElementById("clear-box");
+  clearBoxGenerate.insertBefore(clearBtn, clearBoxGenerate.firstChild);
 
-          // context.canv.setAttribute("id", "canvasID");
-          var red = document.getElementsByClassName('red');
-          var green = document.getElementsByClassName('green');
-          var blue = document.getElementsByClassName('blue');
-        
-            canvas.addEventListener("mousedown", onMouseDown);
+  // context.canv.setAttribute("id", "canvasID");
+  var red = document.getElementsByClassName('red');
+  var green = document.getElementsByClassName('green');
+  var blue = document.getElementsByClassName('blue');
 
-    function onMouseDown(event) {
-        mouseX = event.clientX - rect.left;
-        mouseY = event.clientY - rect.top;
-        canvas.addEventListener("mousemove", onMouseMove);
-        document.body.addEventListener('mouseup', onMouseUp);
-    }
+  canvas.addEventListener("mousedown", onMouseDown);
 
-    function onMouseMove(event) {
-        context.beginPath();
-        context.moveTo(mouseX, mouseY);
-        mouseX = event.clientX - rect.left;
-        mouseY = event.clientY - rect.top;
-        context.lineTo(mouseX, mouseY);
-        context.stroke();
-    }
+  function onMouseDown(event) {
+      mouseX = event.clientX - rect.left;
+      mouseY = event.clientY - rect.top;
+      canvas.addEventListener("mousemove", onMouseMove);
+      document.body.addEventListener('mouseup', onMouseUp);
+  }
 
-    function onMouseUp(event) {
-        canvas.removeEventListener("mousemove", onMouseMove);
-        document.body.removeEventListener( 'mouseup', onMouseUp);
-    }
+  function onMouseMove(event) {
+      context.beginPath();
+      context.moveTo(mouseX, mouseY);
+      mouseX = event.clientX - rect.left;
+      mouseY = event.clientY - rect.top;
+      context.lineTo(mouseX, mouseY);
+      context.fillStyle = red;
+      context.lineWidth = 5;
+      context.stroke();
+  }
+
+  function onMouseUp(event) {
+      canvas.removeEventListener("mousemove", onMouseMove);
+      document.body.removeEventListener( 'mouseup', onMouseUp);
+  }
 }
+
 window.onload = onReady;
 
 
